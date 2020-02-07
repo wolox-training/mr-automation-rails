74b0f492d29a633bb822b93bed365a0f7240bc8e
Mariano Ranucci <mariano.ranucci@wolox.com.ar>
2020-02-07 10:58:04 -0300

    Added unsuccessful scenarios for sign-up

--- features/sign_up.feature
+++ features/sign_up.feature
@@ -10,3 +10,51 @@ Feature: Sign Up
     When I fill all the required fields
     And I click on the submit button
     Then I will be redirected to the login page
+
+  Scenario Outline: Unsuccessful registration: Empty parameters
+    Given I am on the login page
+    And I click on the registration button
+    When I leave the "<field>" field empty
+    And I click some other field
+    And I click on the submit button
+    Then A required field message will pop up for "<field>"
+    And I will not be redirected to the login page
+
+    Examples:
+      | field     |
+      | firstname |
+      | lastname  |
+      | email     |
+      | password  |
+
+  Scenario Outline: Unsuccessful registration: invalid email
+    Given I am on the login page
+    And I click on the registration button
+    When I fill the "email" field with invalid information like: "<information>"
+    And I click some other field
+    And I click on the submit button
+    Then A invalid "email" message will pop up
+    And I will not be redirected to the login page
+
+    Examples:
+      | information                |
+      | invalid_mail               |
+      | invalid mail@wolox.com.ar  |
+      | invalid!&mail@wolox.com.ar |
+
+  Scenario: Unsuccessful registration: invalid password
+    Given I am on the login page
+    And I click on the registration button
+    When I fill the "password" field with invalid information like: "bad password"
+    And I click some other field
+    And I click on the submit button
+    Then A invalid "password" message will pop up
+    And I will not be redirected to the login page
+
+  Scenario: Unsuccessful registration: email already taken
+    Given I am on the login page
+    And I click on the registration button
+    When I fill the required fields but the email already exists in the database
+    And I click on the submit button
+    Then An email already taken error message should pop up
+    And I will not be redirected to the login page
--- features/step_definitions/sign_up.rb
+++ features/step_definitions/sign_up.rb
@@ -1,14 +1,16 @@
-# frozen_string_literal: true
-
 require 'faker'
 
+# Navigation to sign up page:
+
 Given('I am on the login page') do
-  navigate_to('https://woloxbooksv3.firebaseapp.com/es')
+  navigate_to(ENV['URL'])
 end
 
 And('I click on the registration button') do
   click(:id, 'sign-up')
 end
+
+# Successful registration:
 
 When('I fill all the required fields') do
   enter_text(:id, Faker::Name.first_name, 'firstname')
@@ -22,5 +24,57 @@ And('I click on the submit button') do
 end
 
 Then('I will be redirected to the login page') do
-  expect($driver.current_url).to eq('https://woloxbooksv3.firebaseapp.com/es/login')
+  wait(2) # por qué??
+  expect($driver.current_url).to eq(ENV['URL'] + '/login')
+end
+
+# Unsuccessful registration: Empty parameters
+
+When('I leave the {string} field empty') do |field|
+  enter_text(:id, '', field)
+end
+
+And('I click some other field') do
+  click(:id, 'unauth_container')
+end
+
+Then('A required field message will pop up for {string}') do |field|
+  case field
+  when 'firstname' || 'lastname'
+    field = 'first_name_register_span'
+  when 'email' || 'password'
+    field += '_register_span_req'
+  end
+  alert = check_element_presence(:id, field, true)
+  expect(alert).to be(true)
+end
+
+And('I will not be redirected to the login page') do
+  expect($driver.current_url).to eq(ENV['URL'] + '/sign-up')
+end
+
+# Unsuccessful registration: invalid email/password
+
+When('I fill the {string} field with invalid information like: {string}') do |field, information|
+  enter_text(:id, information, field)
+end
+
+Then('A invalid {string} message will pop up') do |field|
+  field += '_register_span'
+  alert = check_element_presence(:id, field, true)
+  expect(alert).to be(true)
+end
+
+# Unsuccessful registration: email already taken
+When('I fill the required fields but the email already exists in the database') do
+  enter_text(:id, Faker::Name.first_name, 'firstname')
+  enter_text(:id, Faker::Name.last_name, 'lastname')
+  enter_text(:id, ENV['EXISTING_EMAIL'], 'email')
+  enter_text(:id, Faker::Internet.password, 'password')
+end
+
+Then('An email already taken error message should pop up') do
+  wait(1)
+  alert = check_element_presence(:id, 'submit_error', true)
+  expect(alert).to be(true)
 end
