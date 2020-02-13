8dbbae58367931f3fc5e9fd6c40e962ecd50a5cc
Mariano Ranucci <mariano.ranucci@wolox.com.ar>
2020-02-13 08:40:04 -0300

    Added after hook

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,9 @@
 Before('@login') do
   click(:id, 'loggout') if $driver.current_url == (ENV['URL'] + '/books')
 end
+
+After do
+  $driver.manage.delete_all_cookies
+  $driver.local_storage.clear
+  sleep 1
+end
